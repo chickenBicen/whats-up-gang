6f79eb97e49426e9c3976fb26bd0101d18312db5
chickenBicen <rds.jr24@gmail.com>
2025-02-18 18:07:46 -0600

    added Grade methods

--- Grade.cpp
+++ Grade.cpp
@@ -1,1 +1,49 @@
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <sstream>
+#include <string>
 
+using namespace std;
+
+class Grade {
+    private:
+        double value;
+
+    public:
+        Grade(){
+            value = 0;
+        }
+
+        Grade(double value){
+            this->value = value;
+        }
+
+        void setValue(double value){
+            this->value = value;
+        }
+
+        string getLetterGrade(){
+            if(value >= 90){
+                return "A";
+            } else if(value >= 80){
+                return "B";
+            } else if(value >= 70){
+                return "C";
+            } else if(value >= 60){
+                return "D";
+            } else {
+                return "F";
+            }
+        }
+
+        double getValue(){
+            return value;
+        }
+
+        string toString(){
+            stringstream ss;
+            ss << value << getLetterGrade();
+            return ss.str();
+        }
+};
